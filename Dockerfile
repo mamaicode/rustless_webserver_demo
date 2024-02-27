44129a0a69aa3be8e4dc03891b3a0da44a8e1ce3
scirin <irine.kokilashvili@gmail.com>
2024-02-28 03:10:12 +0400

    Add comments

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
+# Use rust base image
 FROM rust
-
+# Copy over the source code that we have built in the container
 COPY . .
-
+# Expose and map port 8080 to internal containers 8080
 EXPOSE 8080:8080
-
+# Use cargo, a rust package manager to produce a binary
 RUN cargo build --release
-
+# Run rust as binary that has been produced during the build process from our release folder
 CMD [ "./target/release/rustless" ]
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
 #![deny(warnings)]
 
+// Import necessary modules from the hyper crate
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
+// Import `Infallible` for handling errors
 use std::convert::Infallible;
+// Import `SocketAddr` for defining the server address
 use std::net::SocketAddr;
 
+// Define an asynchronous function `rustless` which handles HTTP requests
 async fn rustless(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    // Respond with "Hello, Rustless!"
     Ok(Response::new("Hello, Rustless!".into()))
 }
 
+// Define the main function using the Tokio runtime
 #[tokio::main]
 async fn main() {
-    // Bind to 127.0.0.1:8080
+    // Define the address to bind the server to (in this case, 127.0.0.1:8080)
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
-    // A `Service` is needed for every connection, so this
-    // creates one from our `rustless` function.
+    // Create a service factory using `make_service_fn`
+    // This will create a new `Service` for each incoming connection
     let make_svc = make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
+        // Convert the `rustless` function into a `Service` using `service_fn`
         Ok::<_, Infallible>(service_fn(rustless))
     });
 
+    // Create a new HTTP server that listens on the specified address
     let server = Server::bind(&addr).serve(make_svc);
 
-    // Run this server for... forever!
+    // Start the server and handle any errors that might occur
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
